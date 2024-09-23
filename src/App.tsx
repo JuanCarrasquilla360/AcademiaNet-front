3e775861b64657e6f1140711aaa0deb5088cd638
Juan Carlos Carrasquilla <juan.carrasquilla@edu.gov.co>
2024-09-23 17:19:14 -0500

    add data grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { FC, ReactNode } from "react";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import { useAuth } from "./hooks/useAuth";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import NotFoundPage from "./pages/NotFoundPage";
+import { Typography } from "@mui/material";
+import InstitutionConfigRouter from "./pages/InstitutionConfig/InstitutionConfigRouter";
+import EnrollmentPeriodRouter from "./pages/EnrollmentPeriod/EnrollmentPeriodRouter";
+import AcademicProgramsRouter from "./pages/AcademicPrograms/AcademicProgramsRouter";
+import AcademicExamsRouter from "./pages/AcademicExams/AcademicExamsRouter";
+import AdminRouter from "./pages/Admin/AdminRouter";
 
-function App() {
-  const [count, setCount] = useState(0)
+const ProtectedRoute: FC<{ children: ReactNode }> = ({ children }) => {
+  const { isAuthenticated } = useAuth();
+  return isAuthenticated ? <>{children}</> : <Navigate to="/logged" />;
+};
 
+const App: FC = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route
+              path="/institution-config/*"
+              element={<InstitutionConfigRouter />}
+            />
+            <Route
+              path="/enrollment-period/*"
+              element={<EnrollmentPeriodRouter />}
+            />
+            <Route
+              path="/academic-programs/*"
+              element={<AcademicProgramsRouter />}
+            />
+            <Route path="/academic-exams" element={<AcademicExamsRouter />} />
+            <Route path="/admin" element={<AdminRouter/>} />
+            <Route path="*" element={<NotFoundPage />} />
+            <Route
+              path="/logged"
+              element={
+                <ProtectedRoute>
+                  <Typography>loggeado</Typography>
+                </ProtectedRoute>
+              }
+            />
+          </Route>
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
